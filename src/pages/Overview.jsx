5715a83195f2d43f784ba7d0208e07a7434e8005
Shuja Ur Rehman <shuja0094@gmail.com>
2024-07-31 16:41:18 +0500

    gmail test-2

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import AverageCard from "../components/card/AverageCard";
 import ChatHistoryTable from "../components/table/ChatHistoryTable";
 import TaskHistory from "../components/overview/TaskHistory";
+import Gmail from "../components/oAuth/Gmail";
 const Overview = () => {
   return (
     <div className="h-[100%] bg-background p-4">
@@ -13,6 +14,7 @@ const Overview = () => {
         <ChatHistoryTable />
         <TaskHistory />
       </div>
+      <Gmail />
     </div>
   );
 };
