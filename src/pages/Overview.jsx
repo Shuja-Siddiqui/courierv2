ab600bdb69970556f3b456eb3cb0cad60e613d67
Shuja Ur Rehman <shuja0094@gmail.com>
2024-07-31 21:17:16 +0500

    remove gmail

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import AverageCard from "../components/card/AverageCard";
 import ChatHistoryTable from "../components/table/ChatHistoryTable";
 import TaskHistory from "../components/overview/TaskHistory";
-import Gmail from "../components/oAuth/Gmail";
+
 const Overview = () => {
   return (
     <div className="h-[100%] bg-background p-4">
